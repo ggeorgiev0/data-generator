3e73b37f386badf14c3a70b779cf7f58094eb4fd
Georgi Georgiev <ggeorgiev@fmsystems.com>
2021-04-09 00:09:58 +0300

    Add time slot controller for date-time configurations

---
+++ src/controllers/TimeSlotController.ts
@@ -0,0 +1,40 @@
+import { Workbooks } from "../enums/workbookPaths";
+import { ITimeSlotConfiguration } from "../interfaces/ITimeSlotConfiguration";
+import { Reader } from "./Reader";
+
+export class TimeSlotController {
+  public static async getRandomActiveFrom(
+    reader: Reader
+  ): Promise<(string | undefined)[]> {
+    const dateWorksheet = await reader.readWorkbook(Workbooks.DATE);
+    const dates = reader.getAllValuesForColumn("FullDate", dateWorksheet);
+    const halfOfAllDates = Math.ceil(dates.length / 2);
+    return dates.splice(0, halfOfAllDates);
+  }
+
+  public static async createTimeSlotConfigurations(
+    reader: Reader
+  ): Promise<ITimeSlotConfiguration[]> {
+    const configurations: ITimeSlotConfiguration[] = [];
+    const dateWorksheet = await reader.readWorkbook(Workbooks.DATE);
+    const timeSlotWorksheet = await reader.readWorkbook(Workbooks.TIME_SLOT);
+
+    const dateIds = reader.getAllValuesForColumn("ID", dateWorksheet);
+    const timeSlotIds = reader.getAllValuesForColumn("ID", timeSlotWorksheet);
+
+    dateIds.forEach((dateId) => {
+      if (dateId) {
+        timeSlotIds.forEach((timeSlotId) => {
+          if (timeSlotId) {
+            configurations.push({
+              dateId: +dateId,
+              timeSlotId: +timeSlotId,
+            });
+          }
+        });
+      }
+    });
+
+    return configurations;
+  }
+}
---
+++ src/interfaces/ITimeSlotConfiguration.ts
@@ -0,0 +1,4 @@
+export interface ITimeSlotConfiguration {
+  dateId: number;
+  timeSlotId: number;
+}
